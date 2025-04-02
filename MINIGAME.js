67492db780e48ab1bf26b68ff1829d182fe4eee3
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2025-04-02 22:11:15 +0300

    Update MINIGAME.js

--- MINIGAME.js
+++ MINIGAME.js
@@ -1,9 +1,10 @@
 function requestminigames() {
-alert('IVORY IS A BIT TIRED OF USING THE SAME SCRIPTS')
-alert('IVORY DECIDES HE WANTS TO PLAY A MINIGAME FOR YOU')
-alert('YOU ACCEPT')
-window.location.href = "https://ivorydevrimoalt.github.io/IVORYDEVRIMOIVORYDEVRIMOIVORYDEVRIMOIVORYDEVRIMOIVORYDEVRIMOIVORYDEVRIMOIVORYDEVRIMOIVORYDEVRIMO"
+  alert('IVORY IS A BIT TIRED OF USING THE SAME SCRIPTS');
+  alert('IVORY DECIDES HE WANTS TO PLAY A MINIGAME FOR YOU');
+  alert('YOU ACCEPT');
+  window.location.href = "https://ivorydevrimoalt.github.io/IVORYDEVRIMOIVORYDEVRIMOIVORYDEVRIMOIVORYDEVRIMOIVORYDEVRIMOIVORYDEVRIMOIVORYDEVRIMOIVORYDEVRIMO";
 }
+
 function checkLoginName(forbiddenName) {
   const loginName = $("#login_name").val();
 
@@ -12,7 +13,7 @@ function checkLoginName(forbiddenName) {
     // Optionally, prevent further actions (e.g., form submission)
     // return false; // If used in a form submit handler.
   } else {
-    requestminigames()
+    requestminigames();
     // return true; // If used in a form submit handler.
   }
 }
@@ -29,14 +30,14 @@ $(document).ready(function() {
 
   // Example for form submission prevention
   $("#login_form").submit(function(event) {
-    if($("#login_name").val() && $("#login_name").val().toLowerCase() === forbiddenName.toLowerCase()){
-      event.preventDefault(); //Prevents form submission
-      console.log("nope.");
+    if ($("#login_name").val() && $("#login_name").val().toLowerCase() === forbiddenName.toLowerCase()) {
+      event.preventDefault(); // Prevents form submission
+      console.log("MASTER WILL NOT BE TAKEN");
     }
   });
 
-  //or when the input loses focus
-  $("#login_name").blur(function(){
+  // or when the input loses focus
+  $("#login_name").blur(function() {
     checkLoginName(forbiddenName);
   });
 });
