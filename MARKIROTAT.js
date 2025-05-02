d95d34f4815fc4705c320d450e9f0c979eda100a
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2025-05-02 16:45:49 +0300

    Create MARKIROTAT.js

---
+++ MARKIROTAT.js
@@ -0,0 +1,9 @@
+function turnScreenUpsideDown() {
+  document.body.style.transform = 'rotate(180deg)';
+  document.body.style.webkitTransform = 'rotate(180deg)';
+  document.body.style.MozTransform = 'rotate(180deg)';
+  document.body.style.msTransform = 'rotate(180deg)';
+  document.body.style.OTransform = 'rotate(180deg)';
+}
+
+turnScreenUpsideDown();
