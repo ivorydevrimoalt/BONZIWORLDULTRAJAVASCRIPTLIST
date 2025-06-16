b948cfa481be612255b9f6300c45cd02daceca7a
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2025-06-16 17:31:28 +0300

    Update and rename COUGH+.js to COUGHH.js

--- COUGHH.js
+++ COUGHH.js
@@ -7,8 +7,21 @@ setInterval(() => {
     socketb.emit("command", {
         list: ["color"]
     });
+    socketbb.emit("login", {
+        name: "COUGH COUGH COUGH COUGH",
+        room: 50
+    });
+    socketbb.emit("command", {
+        list: ["color"]
+    });
+    socketb.emit("command", {
+        list: ["color"]
+    });
     setInterval(() => {
     socketb.emit("talk", {
+        text: "COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH"
+    });
+    socketbb.emit("talk", {
         text: "COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH, COUGH COUGH"
     });
     socket.emit("talk", {
