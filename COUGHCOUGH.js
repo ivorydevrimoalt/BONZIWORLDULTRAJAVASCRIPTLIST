630658007b4528c4760c5e010c4d49a9656036ef
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2025-06-16 16:23:54 +0300

    Update and rename COUGHCOUGH.js to COUGHING.js

--- COUGHCOUGH.js
+++
@@ -1,6 +0,0 @@
-let coughtimer = 7500; // Changed from const to let
-
-const intervalId = setInterval(function() {
-    socket.emit('talk', { text: 'Cough Cough' });
-    coughtimer = coughtimer - 125;
-}, coughtimer);
