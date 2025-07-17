92d9346a7419643bb89058b928e62232d5bb9996
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2025-07-17 04:45:31 +0300

    Update and rename SPRPIC.js to SPRPCT.js

--- SPRPCT.js
+++ SPRPCT.js
@@ -7,7 +7,11 @@ const words = ['Durple',
                'Simon',
                'Tree',
                'Mr fun PC',
+               'Pinki',
+               'Cluckux',
+               'Brud'
               ];
 const randomIndex = Math.floor(Math.random() * words.length);
 const randomWord = words[randomIndex];
-socket.emit('talk', { text: "i am " + randomWord + " from the " + randomWord + "SprunkPicture world"});
+socket.emit('talk', { text: "i am " + randomWord + " from the " + randomWord + " SprunkPicture world"});
+socket.emit('command', { list: ["name", randomWord] });
