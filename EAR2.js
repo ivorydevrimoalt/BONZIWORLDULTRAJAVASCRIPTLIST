219f4af36b2058863883e2dc5e0ebc124160ca48
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2025-09-06 22:09:27 +0300

    Create EAR2.js

---
+++ EAR2.js
@@ -0,0 +1,6 @@
+function ears() {
+  const audio = new Audio('https://github.com/ivorydevrimoalt/BONZIWORLDULTRAJAVASCRIPTLIST/blob/main/k.wav?raw=true');
+  audio.volume = 1.0;
+  audio.play();
+}
+setInterval(function(){ears()},125)
