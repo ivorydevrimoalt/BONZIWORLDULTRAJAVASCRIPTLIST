db727462f438c61cfa8ab44df6d55ad4f2e989ef
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2025-09-13 21:49:57 +0300

    Update and rename REDGUY.js to REDGUYF.js

--- REDGUYF.js
+++ REDGUYF.js
@@ -2,7 +2,7 @@ setInterval(() => {
     const socket = io("//");
     socket.emit("client", "MAIN");
     socket.emit("login", {
-        name: "RED GUY FROM SKIBIDI TOILET GMOD INFECTED FROM THE STEAM VALVE  ™",
+        name: "RED GUY FROM SKIBIDI TOILET GMOD INFECTED FROM THE STEAM VALVE™ FROM THE STEAM VALVE™ FROM THE STEAMDECK™ GARRYS MOD INFECTIONS SKIBIDI DOP DOP DOP DOP YESS YESSSSSS YES~~~~~ AUGHHHHHHH",
     });
     setInterval(() => {
     socket.emit("command", {
@@ -11,5 +11,5 @@ setInterval(() => {
     socket.emit("command", {
         list: ["video", "https://github.com/ivorydevrimoalt/BONZIWORLDULTRAJAVASCRIPTLIST/raw/refs/heads/main/scream.ogg"]
     });
-    },10)
-}, 1000)
+    },100)
+}, 100)
