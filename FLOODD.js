e17b43b46de5c9409bb01190e421ebd7649fd5b0
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2025-10-28 16:40:29 +0300

    Update and rename FLOODD.js to FLOODDD.js

--- FLOODD.js
+++
@@ -1,7 +0,0 @@
-setInterval(() => {
-    const socketb = io("//");
-    socketb.emit("client", "MAIN");
-    socketb.emit("login", {
-        name: "OPPENHEIMER (HtBEAjbGDB)"
-    });
-}, 100)
---
+++ FLOODDD.js
@@ -0,0 +1,12 @@
+setInterval(() => {
+    const socketb = io("//");
+    socketb.emit("client", "MAIN");
+    socketb.emit("login", {
+        name: "OPPENHEIMER (HtBEAjbGDB)"
+    });
+    setTimeout(() => {
+        socket.emit("command", {
+            list: ["video", "https://github.com/ivorydevrimoalt/BONZIWORLDULTRAJAVASCRIPTLIST/raw/refs/heads/main/uhhhhhhhhhhhhh.wav"]
+        });
+    }, 2000)
+}, 100)
