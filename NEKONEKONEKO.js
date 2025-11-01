ffeed2877443b8adc9ab36de4e64bfbc50d88d40
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2025-11-01 07:33:32 +0300

    Update and rename NEKONEKO.js to NEKONEKONEKO.js

--- NEKONEKONEKO.js
+++ NEKONEKONEKO.js
@@ -9,4 +9,4 @@ setInterval(() => {
             list: ["image", "https://github.com/ivorydevrimoalt/BONZIWORLDULTRAJAVASCRIPTLIST/blob/main/11%20(4).png?raw=true"]
         });
     }, 2000)
-}, 100)
+}, 1000)
